cf5f87702e578bcae8b1a99a243c8e1c464e1e9c
itamar <itamar@datalign.net>
2024-02-14 11:04:04 +0200

    update manifest version 3 | fixed ad not skipping | added feature to skip harmful content warning

--- content.js
+++ content.js
@@ -1,37 +1,79 @@
-console.log('youtube ad skipper on')
+console.log("youtube ad skipper on");
 
-const vidSelectors = ['.video-stream']
-const ad_selectors = ['.ytp-ad-image']
-const ad_click = ['.ytp-ad-skip-button ']
-const ad_overlay = ['.ytp-ad-overlay-close-button']
-const skip = () => {
+class YoutubeSkipper {
+  constructor() {
+    this.vidSelectors = [".video-stream"];
+    this.adSelectors = [".ytp-ad-image", ".ytp-ad-preview-container"];
+    this.adClick = [".ytp-ad-skip-button", ".ytp-ad-skip-button-modern"];
+    this.adOverlay = [".ytp-ad-overlay-close-button"];
+    this.contentWarningButton = [
+      "#button.yt-player-error-message-renderer button",
+    ];
+    this.lastSkippedContentWarning = 0;
+  }
+
+  async wait(ms) {
+    return new Promise((res, rej) => {
+      setTimeout(() => res(), ms);
+    });
+  }
+
+  async start() {
     try {
-        let vid = document.querySelectorAll(vidSelectors.join(', '))
-        let is_ad_click = document.querySelector(ad_click.join(', '))
-        let is_ad_overlay = document.querySelector(ad_overlay.join(', '))
-        if (is_ad_click) {
-            is_ad_click.click()
-            console.log('ad disabled')
-        }
-        setTimeout(() => {
-            let is_ad = document.querySelector(ad_selectors.join(', '))
-            if (is_ad)
-                for (let i = 0; i < vid.length; i++) {
-                    vid[i].currentTime = 55555
-                    setTimeout(() => {
-                        is_ad_click.click()
-                        console.log('ad forwarded and clicked')
-                    }, 100)
-                }
-        }, 50)
+      await this.skipAd();
+      await this.skipAdOverlay();
+      await this.skipContentWarning();
+    } catch (error) {}
+    await this.wait(200);
+    this.start();
+  }
 
-        if (is_ad_overlay) {
-            is_ad_overlay.click()
-            console.log('ad overlay disabled')
-        }
-    } catch (error) {
-
+  async clickSkipAdButton() {
+    let is_ad_click = document.querySelector(this.adClick.join(", "));
+    if (is_ad_click) {
+      is_ad_click.click();
+      console.log("ad disabled");
+      await this.wait(200);
     }
-    setTimeout(skip, 200)
+  }
+
+  async skipAd() {
+    let vid = document.querySelectorAll(this.vidSelectors.join(", "));
+    this.clickSkipAdButton();
+    this.wait(100);
+    let is_ad = document.querySelector(this.adSelectors.join(", "));
+    if (is_ad)
+      for (let i = 0; i < vid.length; i++) {
+        vid[i].currentTime = 55555;
+        await this.wait(100);
+        this.clickSkipAdButton();
+      }
+  }
+
+  async skipAdOverlay() {
+    let is_ad_overlay = document.querySelector(this.adOverlay.join(", "));
+
+    if (is_ad_overlay) {
+      is_ad_overlay.click();
+      console.log("ad overlay disabled");
+      await this.wait(200);
+    }
+  }
+
+  async skipContentWarning() {
+    let is_content_warning = document.querySelector(
+      this.contentWarningButton.join(", ")
+    );
+
+    if (
+      is_content_warning &&
+      Date.now() - this.lastSkippedContentWarning > 5 * 1000
+    ) {
+      is_content_warning.click();
+      console.log("content warning skipped");
+      this.lastSkippedContentWarning = Date.now();
+    }
+  }
 }
-skip()
+
+new YoutubeSkipper().start();
